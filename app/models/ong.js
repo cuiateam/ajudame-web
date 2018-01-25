190e83e66ad8813d4243a42e90e801b3fe6e006a
ziguifrido <ziguifrido@gmail.com>
2018-01-25 09:52:24 -0300

    Merge #4
    Não esta aparecendo no teste....

--- app/models/ong.js
+++ app/models/ong.js
@@ -2,7 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   nome: DS.attr('string'),
-  descricao: DS.attr('string'),
+  descricao: DS.attr('string'),  
+  telefone: DS.attr('string'),
+  email: DS.attr('string'),
+  site: DS.attr('string'),
+  facebook: DS.attr('string'),
+  instagram: DS.attr('string'),  
   trabalhos: DS.attr('string'),
   favorecidos: DS.attr('string'),
   endereco: DS.attr('string'),
